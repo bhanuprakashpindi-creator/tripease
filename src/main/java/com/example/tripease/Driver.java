2ff6f956704aa27e425bbe2fb5fcdc700ad76b59
Bhanu Prakash Pindi <bhanuprakashpindi1@gmail.com>
2026-05-21 13:02:08 +0530

    Added customer and driver entities with database setup

---
+++ src/main/java/com/example/tripease/Driver.java
@@ -0,0 +1,21 @@
+package com.example.tripease;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Driver {
+    @Id
+    private int driverId;
+    private String name;
+    private int age;
+    private  String emailId;
+}
